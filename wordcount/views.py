a283567b67b2d8eb914ca972b56c343f7ae034bf
root <root@localhost.localdomain>
2019-04-29 14:57:47 +0530

    adding files

---
+++ wordcount/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(requests):
+    return render(requests, 'home.html')
+
+
+def main(requests):
+    val = requests.POST
+    # print(val)
+    return render(requests, 'main.html', {'val': val})
